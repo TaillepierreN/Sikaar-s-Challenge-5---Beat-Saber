492e5f7bde0c355b3c2ba27bd480ba78983adf2c
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-03-02 15:54:53 +0100

    Started Gameplay mechanics/music mapping

--- Assets/Scripts/MusicMapsManager.cs
+++ Assets/Scripts/MusicMapsManager.cs
@@ -6,7 +6,10 @@ public class MusicMapsManager : MonoBehaviour
 {
     [SerializeField] GameObject[] cubes;
     [SerializeField] Transform[] spawners;
-    
+    [SerializeField] AudioSource musicManager;
+    [SerializeField] List<AudioClip> audioClips;
+    [SerializeField] GameObject[] gameActive;
+    [SerializeField] GameObject[] gameOff;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +20,35 @@ public class MusicMapsManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void PlaySilentRunning()
+    {
+        musicManager.clip = audioClips[0];
+        foreach(GameObject go in gameActive)
+        {
+            go.SetActive(true);
+        }
+        musicManager.Play();
+        StartCoroutine("SilentRunningMap");
+
+    }
+    IEnumerator SilentRunningMap()
+    {   
+        yield return new WaitForSeconds(5.0f);
+        endSong();
+    }
+    void endSong()
+    {
+        foreach(GameObject go in gameActive)
+        {
+            go.SetActive(false);
+        }
+    }
+    void spawnCube(Transform spawnpoint,float rotation,int color)
+    {
+        GameObject cube = Instantiate(cubes[color],spawnpoint);
+        cube.transform.localPosition = Vector3.zero;
+        cube.transform.Rotate(transform.forward,rotation);
     }
 }
